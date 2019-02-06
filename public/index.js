e02daae067c347ab4007b217a612374a916aabfc
Ryan Smith <ryan.l.smith4@gmail.com>
2019-02-06 13:50:38 -0800

    unfunctional websockets

--- public/index.js
+++ public/index.js
@@ -3,5 +3,19 @@ $(document).ready(() => {
   // Connect to socket.io Server
   const socket = io.connect();
 
+  $('#createUserBtn').click((e) => {
+    e.preventDefault();
+    const username = $('#usernameInput').val();
+    if (username.length > 0) {
+      // Emit to server the new user
+      socket.emit('new user', username);
+      $('.usernameForm').remove();
+    }
+  });
 
+
+  // socket Listeners
+  socket.on('new user', (username) => {
+    console.log(`✋ ${username} has joined the chat! ✋`);
+  });
 });
--- server.js
+++ server.js
@@ -3,18 +3,20 @@ const express = require('express');
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 // Socket.io has to use the http Server
 const server = require('http').Server(app);
 // Define handlebars
 const exphbs = require('express-handlebars');
 
-
 // Socket.io
 const io = require('socket.io')(server);
 
+// const sock = require('./sockets/chat.js')(io);
 io.on('Connection', (socket) => {
   console.log(`🔌 New user connected! 🔌 ${socket}`);
+  // sock(io, socket);
+  require('./sockets/chat.js')(io, socket);
 });
 
 // MiddleWare
@@ -28,6 +30,6 @@ app.get('/', (req, res) => {
   res.render('index.handlebars');
 });
 
-server.listen(port, () => {
-  console.log(`Server Listening on ${port}`);
+server.listen(PORT, () => {
+  console.log(`Server Listening on ${PORT}`);
 });
